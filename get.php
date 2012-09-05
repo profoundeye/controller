200765549b94792fdb912e26f81f6fb1baa5b209
zplaying <profoundeye@gmail.com>
2012-09-05 17:48:12 +0800

    抓取数组处理完毕

--- get.php
+++ get.php
@@ -19,7 +19,15 @@ class get extends top{
 	function input(){
 		//还要增加来源出处
 		$info = $this->spArgs('info');
-		$this-> process($info);
+		$this->_host = $this->spArgs('host');
+		$values = $this-> process($info);
+		print_r($values);
+		//curl所有图片信息存在缓存目录
+		
+		//显示修改数组
+		
+		//调用正常photo保存模式
+		
 		$this->display("url_post.html");
 	} 
 	
@@ -27,10 +35,40 @@ class get extends top{
 		$a = preg_split('/<p>(.*?)<\/p>|<h1.*?>(.*?)<\/h1>|<img.*?src="(.*?)".*?>/s',$info,-1,PREG_SPLIT_DELIM_CAPTURE);
 		foreach($a as $i){
 			$temp = trim(preg_replace('/<.*?>/',"",$i));
-			if(!empty($temp)){$output[]=$temp;}
+			if(!empty($temp)){$output['org'][]=$temp;}
 		}
-		print_r($output);
+		
+		$temp = -1;
+		foreach($output['org'] as $k=>$o){
+			$isImg = $this->isImg($o);
+			//如果k=0，标题
+			if($k==0){$output['org']['title']=$o;}
+			if(!$isImg&&$temp==-1&&$k!=0){$output['org']['headline'].=$o;}
+			if($isImg){
+				if(preg_match("/http/is", $o)){
+					$output['imgText'][$k]['img']=$o;
+				}else{
+					$output['imgText'][$k]['img']="http://".$this->_host.$o;
+				}
+				
+				$temp = $k;
+			}
+			if(!$isImg&&$temp!=-1){
+				$output['imgText'][$temp]['text'].=$o;
+			}
+		}
+		return $output;
 	}
+	
+	function isImg($str){
+		if(preg_match("/png|jpeg|jpg|gif/is", $str)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	
 	
 	function kuayu(){
 		//echo '<form method="get" action="http://localhost/testyunbian/index.php?c=get&a=input"><textarea id="info name="info"> </textarea><input type="submit" name="button" id="button" value="提交" /></form>';
